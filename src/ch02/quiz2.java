c85ae7f6b1e8da244ef89ba70d6a3322c7d753ef
wsu063 <wsu063@gmail.com>
2024-02-07 11:43:25 +0900

    first commit

---
+++ src/ch02/quiz2.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class quiz2 {
+    public static void main(String[] args) {
+        System.out.println("1" + "2");
+        System.out.println('A'+ 'B');
+        System.out.println('1' + 2);
+        System.out.println('1' + '2');
+        System.out.println('J' + "ava");
+
+    }
+}
---
+++ src/ch02/quiz3.java
@@ -0,0 +1,32 @@
+package ch02;
+
+public class quiz3 {
+    public static void main(String[] args) {
+        // byte byteValue = 10;
+        // char charValue = 'A';
+        // int intValue = byteValue;
+        // int intValue2 = charValue;
+        // short shortValue = charValue;
+        // double doubleValue = byteValue;
+//        int intValue = 10;
+//        char charValue = 'A';
+//        double doubleValue = 5.7;
+//        String strValue = "A";
+//        char var = (char) strValue;
+//        byte byteValue = 10;
+//        float floatValue = 2.5F;
+//        double doubleValue = 2.5;
+//        byte result =  byteValue + byteValue;
+//        byte b = 256;
+//        char c = '';
+//        char answer = 'no';
+//        double d = 1.4e3f;
+        byte b = 10;
+        char ch = 'A';
+        int i = 100;
+        long l = 1000L;
+        i = ch;
+
+
+    }
+}
