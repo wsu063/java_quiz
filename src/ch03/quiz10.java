b53ea22dab88d38c43e567519c979789389a9703
wsu063 <wsu063@gmail.com>
2024-02-08 14:40:27 +0900

    240208 quiz2 수정

--- src/ch03/quiz10.java
+++ src/ch03/quiz10.java
@@ -3,7 +3,7 @@ package ch03;
 public class quiz10 {
     public static void main(String[] args) {
         int num = 333;
-        System.out.println(num/10*10 + 1);
+        System.out.println(num/10*10 + 1); // 1의자리 수를 0으로 초기화하고, 그자리에 1을 더한다
 
     }
 }
