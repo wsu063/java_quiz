4b45b9286edac0526575d8f63bf9054c82599eaf
wsu063 <wsu063@gmail.com>
2024-02-08 14:55:22 +0900

    12번 수정

--- src/ch03/quiz12.java
+++ src/ch03/quiz12.java
@@ -3,7 +3,7 @@ package ch03;
 public class quiz12 {
     public static void main(String[] args) {
         char ch = 'z';
-        boolean b = ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9');
+        boolean b = ('a' <= ch && ch <= 'z') || (('A' <= ch && ch <= 'Z') ? true : false )|| ('0' <= ch && ch <= '9');
         System.out.println(b);
 
     }
