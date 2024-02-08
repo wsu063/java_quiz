fda3a21b75c80a47078653ee0dc18eed236dd431
wsu063 <wsu063@gmail.com>
2024-02-08 15:06:56 +0900

    13번 수정

--- src/ch03/quiz13.java
+++ src/ch03/quiz13.java
@@ -3,9 +3,11 @@ package ch03;
 public class quiz13 {
     public static void main(String[] args) {
         char ch = 'A';
-        char lowerCase = (char) (ch < 97 ? ch + 32 : ch);
+        char lowerCase = (char) (ch <= 90 ? ch + 32 : ch); // 대문자면 소문자로
+        //'A' = 65, 'Z' = 90, 'a' = 97, 'z' = 122
         System.out.println("ch:"+ch);
         System.out.println("ch to lowerCase:"+lowerCase);
+        System.out.println((int)'a' + "z= "+ (int)'z');
 
     }
 }
