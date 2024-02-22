5f8ff7fe8a1d201260d9e66b3a5be87c473b8769
wsu063 <wsu063@gmail.com>
2024-02-22 16:55:23 +0900

    20240222

---
+++ src/ch07/quiz01to11.java
@@ -0,0 +1,26 @@
+package ch07;
+
+public class quiz01to11 {
+    /*
+    1. 3
+    2. 2 // 자동 타입 변환
+    3. 1 // final 클래스는 상속이 불가능하다.
+    4. 4 // 다른 패키지의 자식 클래스에서 재정의 가능하다.
+    5. 2 // 추상 메소드는 가지든 말든 상관없다.
+    6. child 자식 객체에서 부모클래스의 생성자가 생략 불가능한데 생략했다.
+      => public Child(String name, int studentNo) {
+            super(name);
+            this.studentNo = studentNo
+        }
+    7. Parent(String nation) call
+       Parent() call
+       Child(String name) call
+       Child() call
+    8. 스노우 타이어가 굴러갑니다.
+       스노우 타이어가 굴러갑니다.
+    9. 2 // 첫번째 변수 대입때 자동 형변환이 되어있지 않아서 강제 형변환이 불가능하다.
+    10. 자식 클래스인 Computer에서 추상 메소드 publc abstract void work()를 구현하지 않았다.
+    11. super
+    
+     */
+}
