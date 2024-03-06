82c001dd123f8fe848ef19e7d938f0a4545acee6
wsu063 <wsu063@gmail.com>
2024-03-06 13:52:43 +0900

    수업

--- src/ch12/quiz07/IntegerCompareExample.java
+++ src/ch12/quiz07/IntegerCompareExample.java
@@ -9,7 +9,7 @@ public class IntegerCompareExample {
 
         System.out.println(obj1.equals(obj2)); // true
         System.out.println(obj3.equals(obj4)); // false
-        // 밑에꺼가 false가 나왔던 이유는 Integer타입의 값이 -127 ~ 128이여서
+        // 밑에꺼가 false가 나왔던 이유는 Integer타입의 값이 -128 ~ 127이여서
         // 범위 밖을 벗어나서 값 비교가 안된다.
     }
 }
